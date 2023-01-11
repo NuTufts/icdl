81d1ba6215f4e1f0eb5718eb9010fc382f0a7458
Taritree Wongjirad <taritree@mit.edu>
2023-01-11 10:15:23 -0600

    bump larlite

--- scripts/setup_sbndgpvm.sh
+++ scripts/setup_sbndgpvm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh
-setup sbndcode v09_50_00 -q e20:prof
-setup cmake v3_22_2
+setup sbndcode v09_64_01 -q e20:prof
+unsetup larcv2
+setup cetmodules v3_20_00
+#setup cmake v3_22_2
